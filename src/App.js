a07632a9e433c7f007ce9fd923cb6630b6b1e7a0
zeerakb1 <23100139@lums.edu.pk>
2023-11-25 23:11:18 -0800

    chatbot template

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import ChatBot from "./Chatbot";
 
 function App() {
+  // const [message, setMessage] = useState([]);
+  // const [text, setText] = useState("");
+
+  // console.log(message);
+
+  // const getResponse = async () => {
+  //   const response = await fetch(`http://localhost:8000/prompt/${text}`);
+  //   const data = await response.json();
+
+  //   console.log(data);
+
+  //   setMessage([
+  //     ...message,
+  //     {
+  //       author: data.messages[0].content,
+  //       bot: data.candidates[0].content,
+  //     },
+  //   ]);
+  // };
+
+  // console.log(text);
+
   return (
-    <div className="App">
-      
-    </div>
+    <ChatBot/>
+
   );
 }
 
---
+++ src/Chatbot.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import './chatbot.css'
+
+const ChatBot = () => {
+    const [messages, setMessages] = useState([]);
+    const [text, setText] = useState("");
+
+    console.log(messages);
+
+    const getResponse = async (e) => {
+        e.preventDefault();
+        const response = await fetch(`http://localhost:8000/prompt/${text}`);
+        const data = await response.json();
+
+        console.log(data);
+
+        setMessages([
+        ...messages,
+        {
+            author: text,
+            bot: data.candidates[0].content,
+        },
+        ]);
+        setText("");
+    };
+
+    return (
+        <div className="chatbot-container">
+            <div className="chat-window">
+                {messages.map((message, index) => (
+                    <React.Fragment key={index}>
+                        <div className="message user">{message.author}</div>
+                        <div className="message bot">{message.bot}</div>
+                    </React.Fragment>
+                ))}
+            </div>
+            <form className="message-form" onSubmit={getResponse}>
+                <input
+                    type="text"
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                    placeholder="Type a message..."
+                />
+                <button type="submit">Send</button>
+            </form>
+        </div>
+    );
+};
+
+export default ChatBot;
